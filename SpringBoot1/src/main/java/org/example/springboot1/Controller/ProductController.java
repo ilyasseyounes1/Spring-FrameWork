985bd07da0aee5f1ef56c0295452d7627aa6513a
ilyasseyounes <ilyasseyounes1@gmail.com>
2024-09-30 10:06:57 +0100

    Spring boot project part2

--- SpringBoot1/src/main/java/org/example/springboot1/Controller/ProductController.java
+++ SpringBoot1/src/main/java/org/example/springboot1/Controller/ProductController.java
@@ -1,14 +1,28 @@
 package org.example.springboot1.Controller;
 
+import org.example.springboot1.Model.Product;
 import org.example.springboot1.Service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
 public class ProductController{
     @Autowired
-    ProductService service ;
-    @RequestMapping("/products")
-    public String getProducts (){
-        return "";
+    ProductService service;
+
+    @GetMapping("/products")
+    public String getProducts () {
+        return service.getProducts ().toString ();
+    }
+
+    @GetMapping("/products/{prodId}")
+    public Product getProductById ( @PathVariable int prodId ) {
+        return service.getProductById ( prodId );
+    }
+
+    // all methode are  by default, it s GET
+    @PostMapping("/products")
+    public void addProduct ( Product prod ) {
+        service.addProduct ( prod );
     }
 }
